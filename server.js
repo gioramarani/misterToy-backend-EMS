bb79ff92a61a0c8dfb9c86c3c4362b91d2a7ae27
Giora Marani <giora.marani@gmail.com>
2023-07-18 19:11:59 +0300

    load to render

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from 'path'
 
 import { toyService } from './services/toy.service.js'
 import { loggerService } from './services/logger.service.js'
